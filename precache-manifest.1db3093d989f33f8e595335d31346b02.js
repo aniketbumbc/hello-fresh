de8371d5fd6efcf6585c89ad21523b500d223b53
aniket <aniket.umbc@gmail.com>
2023-03-22 13:00:25 +0530

    Updates

---
+++ precache-manifest.1db3093d989f33f8e595335d31346b02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4432aebdd4e3bd17de63d0aaf165004d",
+    "url": "/hello-fresh/index.html"
+  },
+  {
+    "revision": "f3516b6ee1b7033041d0",
+    "url": "/hello-fresh/static/js/2.fdccfc1f.chunk.js"
+  },
+  {
+    "revision": "82e8035e49740f791fa94175bc04ac4e",
+    "url": "/hello-fresh/static/js/2.fdccfc1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f59c0c5a8c710fc5df61",
+    "url": "/hello-fresh/static/js/main.601f7d3c.chunk.js"
+  },
+  {
+    "revision": "7fb21ff01d5451cac2fb",
+    "url": "/hello-fresh/static/js/runtime-main.424bdd83.js"
+  }
+]);
